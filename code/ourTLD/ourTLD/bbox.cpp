2611bc9a57275f80dd09b0978fba6caec382cda7
Ruotian Luo <fairylrt@gmail.com>
2014-07-21 21:09:14 +0800

    code change

--- code/ourTLD/ourTLD/bbox.cpp
+++ code/ourTLD/ourTLD/bbox.cpp
@@ -1,16 +1,16 @@
 #include"common.h"
 #include"Config.h"
 namespace TLD{
-	Mat bb_points(Rect bb,int numM,int numN,float margin){
+	Mat bb_points(Mat bb,int numM,int numN,float margin){
 		return Mat();
 	};
-	Rect bb_predict(Rect BB0,Mat pt0,Mat pt1){
-		return Rect();
+	Mat bb_predict(Mat BB0,Mat pt0,Mat pt1){
+		return Mat();
 	}
-	bool bb_isdef(Rect BB2){
+	bool bb_isdef(Mat BB2){
 		return true;
 	}
-	bool bb_isout(Rect BB2,Size imgsize){
+	bool bb_isout(Mat BB2,Size imgsize){
 		return false;
 	}
 }
